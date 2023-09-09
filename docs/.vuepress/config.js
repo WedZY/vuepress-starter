349604baf1209fa01806d491e6039c1f4f6a5e63
星期三 <randellc308@gmail.com>
2023-09-09 13:33:49 +0800

    测试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,33 +1,28 @@
 import { defineUserConfig } from 'vuepress';
 import { recoTheme } from 'vuepress-theme-reco';
 
-// 头部列表模块归类
 export const navbar = [
   { text: '主页', link: '/' },
   {
     text: '我的',
     children: [
-      {
-        text: 'github',
-        icon: 'icon如需配置请看下面链接，如不需要请去掉此字段',
-        link: 'https://github.com/WedZY',
-      },
+      { text: 'github', link: 'https://github.com/code-lucky' },
       { text: '稀土掘金', link: 'https://juejin.cn/user/62036934154445' },
     ],
   },
+  // { text: '留言板', link: '/1',},
+  // { text: '掘金自动签到', link: 'http://124.222.178.74/juejin/login.html',},
 ];
 
 export default defineUserConfig({
-  title: '星期三记录屋', // 头部左侧title
+  title: 'LuckyCode Blog', // 头部左侧title
   theme: recoTheme({
-    home: '/README.md',
     series: {
       // 设置是左侧边栏（sideBar） 还是系列（series）
       '/vuepress-theme-reco/': ['introduce', 'usage'],
     },
-
     navbar, // 顶部或者左侧导航栏
-    author: '星期三', // 作者名字
-    authorAvatar: '/avatar.jpg', // 作者头像(静态文件路径 /docs/.vuepress/public)
+    author: 'LuckyCode', //作者名字
+    authorAvatar: '/avatar.jpg', //作者头像
   }),
 });
--- docs/README.md
+++ docs/README.md
@@ -3,11 +3,10 @@ home: true
 modules: # 指定首页展示模块
   - Banner
   - Blog
-  - MdContent
-  - Footer
 banner: # banner 模块的配置
-  heroText: 星期三记录屋
-  tagline: 用于记录日常开发发现的问题及新知识的学习
+  heroText: LuckyCode #标题
+  tagline: '用于记录日常开发发现的问题及新知识的学习' #简诉
+  heroImage: ''
   heroImageStyle:
     maxWidth: 200px
     margin: 0 auto 2rem
@@ -16,11 +15,5 @@ banner: # banner 模块的配置
     height: 100vh
 blog: # blog 模块的配置
   socialLinks: # 社交地址
-    - { icon: 'BrandGithub', link: 'https://github.com/recoluan' }
-footer: # 底部模块的配置
-  record: 域名备案文案
-  recordLink: 域名备案地址
-  cyberSecurityRecord: 公安备案文案
-  cyberSecurityLink: 公安备案地址
-  startYear: 2018
+    - { icon: 'BrandGithub', link: 'https://github.com/code-lucky' }
 ---
